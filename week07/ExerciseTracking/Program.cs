319b9790b52dec0207c64006e0deb1ecf3ef552c
Cdottt <cdotgattud@gmail.com>
2025-12-11 15:39:14 +0800

    updated 11-12-2025

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("11 Dec 2025", 30, 3.0));
+        activities.Add(new Cycling("11 Dec 2025", 30, 12.0));
+        activities.Add(new Swimming("11 Dec 2025", 30, 40));
+
+        foreach (Activity a in activities)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
     }
 }
